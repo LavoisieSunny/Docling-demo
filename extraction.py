83b5cff6a920050cd42ca5e66cd456e9a5a21275
LavoisieSunny <lavoisiesunnycourse@gmail.com>
2026-06-09 12:47:02 +0530

    first commit

---
+++ extraction.py
@@ -0,0 +1,14 @@
+from docling.document_converter import DocumentConverter
+from utiles.sitemap import get_sitemap_urls
+
+converter = DocumentConverter()
+
+#Basic PDF Extraction
+
+result = converter.convert("pdfs/MA_20_2026.pdf")
+
+document = result.document
+markdown_output = document.export_to_markdown()
+json_output = document.export_to_dict()
+
+print(document)
